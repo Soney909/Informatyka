fff43394116ee66adac3f86f297fa32eac9faae0
Jakub Piskorowski <jakub@gmail.com>
2022-03-30 11:04:48 +0200

    Primie numbers task committed counter added

--- LiczbyPierwsze/Primies/Primies.cpp
+++ LiczbyPierwsze/Primies/Primies.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#include <time.h>
 using namespace std;
 
 int main()
@@ -7,23 +7,30 @@ int main()
     bool flag;
     int lp = 0;
     int p = 2;
+    int c = 0;
     int n;
-    cout << "enter numbers: " << endl;
+    cout<<"enter numbers: "<<endl;
     cin >> n;
 
-    while (lp < n) {
+    clock_t tStart = clock();
+    
+    while(lp < n){
+        c++;
         flag = true;
-        for (int d = 2; d <= p - 1; d++) {
-            if (p % d == 0) {
-                flag = false;
+        for(int d = 2; d <= p-1; d++){
+            if(p % d == 0){
+               flag = false;
             }
         }
-        if (flag == true) {
-            cout << p << endl;
+        if(flag == true){
+            cout<<p<<endl;
             lp++;
+      
         }
         p++;
     }
-
+    cout << "Liczba kroków: ";
+    cout << c << endl;
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
     return 0;
 }
