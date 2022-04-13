07dea2a44fed23868d0d058b655b886ac29547b9
Jakub Piskorowski <jakub@gmail.com>
2022-04-13 10:35:02 +0200

    NWD NWW Commited

---
+++ NWD/NWD.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberOne;
+    int numberTwo;
+    cout << "Podaj pierwszą liczbę: " << endl;
+    cin >> numberOne;
+    cout << "Podaj drugą liczbę: " << endl;
+    cin >> numberTwo;
+    while (numberOne != numberTwo) {
+        if (numberOne < numberTwo) {
+            numberTwo = numberTwo - numberOne;
+        }
+        else {
+            numberOne = numberOne - numberTwo;
+        }
+    }
+    cout << numberOne << endl;
+}
---
+++ NWW/Nww.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberOne;
+    int numberTwo;
+    cout << "Podaj pierwszą liczbę: " << endl;
+    cin >> numberOne;
+    cout << "Podaj drugą liczbę: " << endl;
+    cin >> numberTwo;
+    int mult = numberOne * numberTwo;
+    int nww;
+    while (numberOne != numberTwo) {
+        if (numberOne < numberTwo) {
+            numberTwo = numberTwo - numberOne;
+        }
+        else {
+            numberOne = numberOne - numberTwo;
+        }
+    }
+    nww = mult / numberOne;
+    cout << nww << endl;
+}
